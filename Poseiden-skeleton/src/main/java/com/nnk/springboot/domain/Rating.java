90d5016851d1d5a0d541908cdf9e8190d3c61ee6
bruel.adrien <bruel.adrienpro@gmail.com>
2025-12-26 16:11:18 +0100

    La classe Rating.java a été codé. L'entité est prête et complète.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,34 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "rating")
+@Table(name = "Rating")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Integer id;
+
+    @Size(max = 125)
+    @Column(name = "moodysRating", length = 125)
+    private String moodysRating;
+
+    @Size(max = 125)
+    @Column(name = "sandPRating", length = 125)
+    private String sandPRating;
+
+    @Size(max = 125)
+    @Column(name = "fitchRating", length = 125)
+    private String fitchRating;
+
+    @Column(name = "orderNumber")
+    private Integer orderNumber;
 }
