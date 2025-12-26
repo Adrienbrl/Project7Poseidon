4bc766373bf6ed266328b6abe0c35ae8798186ee
bruel.adrien <bruel.adrienpro@gmail.com>
2025-12-26 16:19:51 +0100

    La classe RuleName.java a été codé. L'entité est prête et complète.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -1,11 +1,43 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import java.sql.Timestamp;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "rulename")
+@Table(name = "RuleName")
+@NoArgsConstructor
+@Getter
+@Setter
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Integer id;
+
+    @Size(max = 125)
+    @Column(name = "name", length = 125)
+    private String name;
+
+    @Size(max = 125)
+    @Column(name = "description", length = 125)
+    private String description;
+
+    @Size(max = 125)
+    @Column(name = "json", length = 125)
+    private String json;
+
+    @Size(max = 512)
+    @Column(name = "template", length = 512)
+    private String template;
+
+    @Size(max = 125)
+    @Column(name = "sqlStr", length = 125)
+    private String sqlStr;
+
+    @Size(max = 125)
+    @Column(name = "sqlPart", length = 125)
+    private String sqlPart;
 }
